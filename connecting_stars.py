6a6ef21458100eca7b7e5fef712ab9a8f79b5354
CyberWarrior404 <mdmor@icloud.com>
2024-12-06 16:58:25 +0400

    Add files via upload

--- connecting_stars.py
+++ connecting_stars.py
@@ -3,13 +3,37 @@ import random
 
 WIDTH = 600
 HEIGHT = 400
+noofstar=7
+stars=[]
+lines=[]
+for i in range(noofstar):
+    Star = Actor("star")    
+    Star.pos = random.randint(50,550),random.randint(50,350)
+    stars.append(Star)
 
-Star = Actor("star")
-Star.pos = random
 
 def draw():
     screen.blit("bg",(0,0))
-    Star.draw()
+    number=1
+    for i in (stars):
+        i.draw()
+        screen.draw.text(str(number),(i.x-5,i.y+10),color="orange")
+        number+=1
+    for line in lines:
+        screen.draw.line(line[0],line[1],color="yellow")
+
+next=0
+
+def on_mouse_down(pos):
+    global lines,stars,next
+    if stars[next].collidepoint(pos):
+        if next:
+            lines.append((stars[next-1].pos, stars[next].pos))
+            next+=1
+        else:
+            lines=[]
+            next=0
+
 
 
 pgzrun.go()
